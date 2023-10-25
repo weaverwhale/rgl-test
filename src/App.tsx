7417506a18cdfe7e3d135df08e3a8114dc31c018
Mike Weaver <michael@triplewhale.com>
2023-10-25 11:16:04 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Rgl } from './RGL'
+
+function App() {
+  return (
+    <>
+      <h1>RGL Demo</h1>
+      <Rgl />
+    </>
+  )
+}
+
+export default App
---
+++ src/RGL.tsx
@@ -0,0 +1,62 @@
+import { useState, useMemo } from 'react'
+import RGL, { WidthProvider, Layout } from 'react-grid-layout'
+import 'react-grid-layout/css/styles.css'
+import './RGL.css'
+
+const initTiles = [...Array(10).keys()].map(
+  (_v, i) =>
+    ({
+      i: i.toString(),
+      x: (i * 4) % 12,
+      y: (i * 4) % 12,
+      w: 4,
+      h: 4,
+    } as Layout),
+)
+
+// This one has an implicit return
+export const Rgl = () => {
+  const ResponsiveGridLayout = useMemo(() => WidthProvider(RGL), [])
+  const [tiles, setTiles] = useState(initTiles)
+
+  return (
+    <div>
+      <button onClick={() => setTiles(initTiles)}>Reset</button>
+      <button
+        style={{ marginLeft: '1em' }}
+        onClick={() => {
+          const newTile = {
+            i: tiles.length.toString(),
+            x: (tiles.length * 4) % 12,
+            y: tiles.length * 4,
+            w: 4,
+            h: 4,
+          }
+
+          setTiles([...tiles, newTile])
+        }}
+      >
+        Add Tile
+      </button>
+      <ResponsiveGridLayout
+        isDraggable
+        isBounded
+        onLayoutChange={(l) => {
+          setTiles(l)
+        }}
+        layout={tiles}
+        autoSize={true}
+        rowHeight={1}
+        margin={[16, 16]}
+      >
+        {tiles.map((tile, i) => (
+          <div className="dashboard-item" key={i} data-grid={tile}>
+            Dashboard ITEM {i}
+          </div>
+        ))}
+      </ResponsiveGridLayout>
+    </div>
+  )
+}
+
+export default Rgl
